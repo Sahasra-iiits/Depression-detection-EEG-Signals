dd22d846afb0f0f1f4e9dc752e19b2518f13f472
Sahasra-iiits <sahasra.g24@iiits.in>
2026-09-22 14:31:09 +0530

    Connecting backend and frontend

--- backend/index.js
+++ backend/index.js
@@ -19,7 +19,7 @@ mongoose
   .catch((err) => console.log(err));
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://depression-detection-eeg-signals.vercel.app/",
     credentials: true,
   }),
 );
